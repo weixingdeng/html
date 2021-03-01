69b69067c002dddf313c1da91e76450c15f81caa
dengwx <deng.dwx@alibaba-inc.com>
2021-03-01 20:07:56 +0800

    init

---
+++ card/card.js
@@ -0,0 +1,137 @@
+var httpUrl = 'http://203.156.220.91:20076';
+new gridjs.Grid({
+    columns: [{
+        name: '银行',
+        attributes: (cell) => {
+            if (cell) {
+                return {
+                    'data-cell-content': cell,
+                    'onclick': () => updateMoney(cell),
+                    'style': 'cursor: pointer',
+                };
+            }
+        }
+    }
+        , '倒计时', '账单日', '还款日',
+    {
+        name: 'nextBill',
+        sort: {
+            enable: true,
+            compare: (a, b) => {
+                const code = (x) => Number(x);
+
+                if (code(a) > code(b)) {
+                    return 1;
+                } else if (code(b) > code(a)) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }
+        }
+    },
+    {
+        name: 'nextReturn',
+        sort: {
+            enable: true,
+            compare: (a, b) => {
+                const code = (x) => Number(x);
+
+                if (code(a) > code(b)) {
+                    return 1;
+                } else if (code(b) > code(a)) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }
+        }
+    },
+        '额度', '负债',
+        '可用额度'
+    ],
+    sort: false,
+    server: {
+        url: httpUrl + '/cardInfo',
+        then: data => data
+    },
+    style: {
+        table: {
+        },
+        th: {
+            'text-align': 'center'
+        },
+        td: {
+            'text-align': 'center'
+        }
+    }
+}).render(document.getElementById("wrapper"));
+
+
+function updateMoney(str) {
+
+    var person = prompt("请输入" + str + '的负债😂😭😂', "");
+    if (person != null && person != "") {
+        sendRequest(str, Number(person));
+    }
+}
+
+function sendRequest(str, money) {
+    var url = httpUrl + "/updateMoney?name=" + str + '&money=' + money/*json文件url，本地的就写本地的位置，如果是服务器的就写服务器的路径*/
+    var request = new XMLHttpRequest();
+    request.open("get", url);/*设置请求方法与路径*/
+    request.send(null);/*不发送数据到服务器*/
+    request.onload = function () {/*XHR对象获取到返回信息后执行*/
+        location.reload();
+    }
+}
+
+window.onload = function () {
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var url = httpUrl + "/totalInfo"/*json文件url，本地的就写本地的位置，如果是服务器的就写服务器的路径*/
+    var request = new XMLHttpRequest();
+    request.open("get", url);/*设置请求方法与路径*/
+    request.send(null);/*不发送数据到服务器*/
+    request.onload = function () {/*XHR对象获取到返回信息后执行*/
+        if (request.status == 200) {/*返回状态为200，即为数据获取成功*/
+            var per = JSON.parse(request.responseText);
+            console.log('请求结果', per);
+            var myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: per.time,
+                    datasets: [{
+                        label: 'Money',
+                        data: per.money,
+                        backgroundColor: [
+                            // 'rgba(255, 99, 132, 0.2)',
+                            // 'rgba(54, 162, 235, 0.2)',
+                            // 'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            // 'rgba(255,99,132,1)',
+                            // 'rgba(54, 162, 235, 1)',
+                            // 'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ card_service.js
@@ -0,0 +1,201 @@
+const { request } = require('express');
+var express = require('express');
+var app = express();
+const cors = require('cors');
+app.use(cors());
+
+// 查询
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: '203.156.220.91',
+  user: 'root',
+  password: '123456',
+  port: '20077',
+  database: 'CARD'
+});
+
+app.get('/cardInfo', function (req, res) {
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  var sql = 'SELECT * FROM visa';
+  connection.query(sql, function (err, result) {
+    if (err) {
+      console.log('[SELECT ERROR] - ', err.message);
+      res.end(err);
+      return;
+    }
+    res.end(resultTreatment(result));
+  });
+})
+
+app.get('/totalInfo',function(req,res){
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  var sql = 'SELECT * FROM total';
+  connection.query(sql, function (err, result) {
+    if (err) {
+      console.log('[SELECT ERROR] - ', err.message);
+      res.end(err);
+      return;
+    }
+    var times = result.map(bank => bank.time);
+    var totalMoney = result.map(bank => bank.total_money);
+    var formatData = {'time':times,'money':totalMoney};
+    res.end(JSON.stringify(formatData));
+  });
+})
+
+app.get('/updateMoney',function(req,res){
+  var sql = 'UPDATE visa SET useMoney = ? WHERE name = ?';
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  var name = req.query.name;
+  var money = req.query.money;
+  connection.query(sql,[money,name],function(err, result){
+    if(err){
+      console.log('[SELECT ERROR] - ', err.message);
+      res.end(err);
+      return;
+    }
+    res.end('ok')
+  })
+
+})
+
+// 将当前时间和总负债存
+app.get('/job_calculate',function(req,res){
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  var sumSQL = 'SELECT SUM(useMoney) as sumMoney FROM visa;'
+  connection.query(sumSQL,function(err,result){
+    if (err) {
+      // console.log('[SELECT ERROR] - ', err.message);
+      res.end(err);
+      return;
+    }
+    if(result.length < 1){
+      res.end(err);
+    }
+    var toalMoney = result[0].sumMoney;
+    var nowTime = dateFormat('YYYY-mm-dd',new Date());
+    var jobCalSql = 'INSERT INTO total (time,total_money) VALUES (?,?)';
+    connection.query(jobCalSql,[nowTime, toalMoney],function(err,result){
+      if(err){
+        // console.log('插入失败',err);
+        res.end(err);
+        return;
+      }
+      res.end('ok');
+    })
+  })
+})
+
+
+
+
+var server = app.listen(10051, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+  console.log('运行中');
+})
+
+// 私有方法
+
+function dateFormat(fmt, date) {
+  let ret;
+  const opt = {
+    "Y+": date.getFullYear().toString(),        // 年
+    "m+": (date.getMonth() + 1).toString(),     // 月
+    "d+": date.getDate().toString(),            // 日
+    "H+": date.getHours().toString(),           // 时
+    "M+": date.getMinutes().toString(),         // 分
+    "S+": date.getSeconds().toString()          // 秒
+    // 有其他格式化字符需求可以继续添加，必须转化成字符串
+  };
+  for (let k in opt) {
+    ret = new RegExp("(" + k + ")").exec(fmt);
+    if (ret) {
+      fmt = fmt.replace(ret[1], (ret[1].length == 1) ? (opt[k]) : (opt[k].padStart(ret[1].length, "0")))
+    };
+  };
+  return fmt;
+}
+
+function dayDifference(day1, day2) {
+  let day = 24 * 60 * 60 * 1000;
+  try {
+    let startTime = day1.getTime();
+
+    let startTime2 = day2.getTime();
+
+    let cha = parseInt(Math.abs(startTime - startTime2) / day);
+    return cha;
+  }
+  catch (e) {
+    return false;
+  }
+}
+
+function getSum(total, num) {
+  return total + Math.round(num);
+}
+
+function resultTreatment(result) {
+  for (var i in result) {
+    var bank = result[i];
+    bank.remainMoney = 0;
+    if (bank.type == 1 || bank.billDay == null || bank.returnDay == null) {
+      continue;
+    }
+    var nowDate = new Date();
+    var billDay = Number(bank.billDay);
+    var returnDay = Number(bank.returnDay);
+    var nowDay = nowDate.getDate();
+
+    // 账单日
+    var billDate = new Date();
+    billDate.setDate(billDay);
+    if (nowDay > billDay) {
+      billDate.setMonth(nowDate.getMonth() + 1);
+    }
+
+    // 还款日
+    var returnDate = new Date(billDate);
+    returnDate.setDate(returnDay);
+    if (billDay > returnDay) {
+      returnDate.setMonth(billDate.getMonth() + 1);
+    }
+    // 距离账单日
+    var billDayDiff = dayDifference(nowDate, billDate);
+    bank.billDayDiff = billDayDiff;
+    bank.billDay = dateFormat('mm-dd', billDate);
+    // 距离还款日
+    var returnDayDiff = dayDifference(nowDate, returnDate);
+    bank.returnDayDiff = returnDayDiff;
+    bank.returnDay = dateFormat('mm-dd', returnDate);
+
+    // 可用额度
+    bank.remainMoney = bank.totalMoney - bank.useMoney;
+
+    // 本月还款倒计时
+    var nowReturnDate = new Date();
+    nowReturnDate.setDate(returnDay);
+    if(nowDay > returnDay){
+      nowReturnDate.setMonth(nowDate.getMonth() + 1);
+    }
+    var diff = dayDifference(nowDate, nowReturnDate);
+    bank.countDownDay = diff;
+  }
+  // 增加一条总额
+  var totalMoney = result.reduce((preTotal, bank) => preTotal + bank.totalMoney, 0);
+  var useMoney = result.reduce((preTotal, bank) => preTotal + bank.useMoney, 0);
+  var remainMoney = result.reduce((preTotal, bank) => preTotal + bank.remainMoney, 0);
+  var total = {
+    "name": "总计",
+    "totalMoney": totalMoney,
+    "useMoney": useMoney, "remainMoney": remainMoney
+  };
+  result.push(total);
+
+  result = result.map(bank => [bank.name, bank.countDownDay, bank.billDay, bank.returnDay, bank.billDayDiff, bank.returnDayDiff, bank.totalMoney, bank.useMoney, bank.remainMoney])
+  // console.log('[SELECT SUCCESS] -', result);
+  return JSON.stringify(result);
+}
+
